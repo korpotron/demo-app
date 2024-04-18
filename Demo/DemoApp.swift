36774d7239414912166aea93a0bffbe6bed854ed
Damian Rzeszot <damian.rzeszot@toyotaconnected.eu>
2024-04-18 22:29:09 +0200

    Initial

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Damian Rzeszot on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
